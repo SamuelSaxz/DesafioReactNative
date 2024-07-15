7d7611c92ce0d825e1dd5bb56604a37e188a0870
Samuel de Almeida Aguilar <samueldealmeida.aguilar12@gmail.com>
2024-07-15 12:32:48 -0300

    Adicionando Provider para Passar as informações dentro do Redux por toda a aplicação e Adicionando Novas Rotas
    Essas novas Duas Rotas eu ainda estou pensando em como posso fazer para deixá-las melhor, ainda mas elas contém as mesmas informações de outras páginas, então devo fazer uma lógica para resolver isso.

--- my-app/src/app/_layout.tsx
+++ my-app/src/app/_layout.tsx
@@ -1,11 +1,18 @@
+// Functions React Native and Expo Router
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Slot, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, Roboto_400Regular, Roboto_300Light, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import theme from '../theme';
+
+// Theme
+import theme from '@/src/theme';
+
+// Store - React Redux
+import store from '@/src/store';
+
+// React Redux
 import { Provider } from 'react-redux';
-// import { store } from '../store';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -22,7 +29,7 @@ export default function Layout() {
   }
 
   return (
-    // <Provider store={store}>
+    <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <StatusBar style="light" />
         {fontsLoaded &&
@@ -35,10 +42,12 @@ export default function Layout() {
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="details" options={{ title: "Detalhes" }} />
+            <Stack.Screen name="searchProfile" options={{ title: "Perfil do Usuário" }} />
+            <Stack.Screen name="searchRepository" options={{ title: "Repositório do Usuário" }} />
           </Stack>
         }
       </GestureHandlerRootView>
-    // </Provider>
+    </Provider>
   );
 }
 
