545ee20f651cf6fa74ff95b94d26f8c1412e8c4b
Samuel de Almeida Aguilar <samueldealmeida.aguilar12@gmail.com>
2024-07-15 12:39:41 -0300

    Arquivo que guarda em si todos os dados que preciso dentro do app.

--- my-app/src/store.tsx
+++ my-app/src/store.tsx
@@ -1,5 +1,72 @@
-// import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-// export const store = configureStore({
-//   reducer: {},
-// });
+const initialState = {
+  user: {
+    id: 0,
+    avatar_url: "",
+    name: "",
+    email: "",
+    bio: "",
+    followers: 0,
+    following: 0,
+  },
+  repository: [],
+  searchUser: {
+    id: 0,
+    avatar_url: "",
+    name: "",
+    email: "",
+    bio: "",
+  },
+  searchRepository: [],
+  // Index para usar o Objeto que é o Repositório.
+  detailsRepository: {},
+};
+
+function reducer(state = initialState, action: any) {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case "SET_REPOSITORY":
+      return {
+        ...state,
+        repository: action.payload,
+      };
+    case "SET_SEARCHUSER":
+      return {
+        ...state,
+        searchUser: action.payload,
+      };
+    case "SET_SEARCHREPOSITORY":
+      return {
+        ...state,
+        searchRepository: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        user: initialState.user,
+        repository: initialState.repository,
+        searchUser: initialState.searchUser,
+        searchRepository: initialState.searchRepository,
+        detailsRepository: initialState.detailsRepository,
+      };
+    case "SET_DETAILSREPOSITORY":
+      return {
+        ...state,
+        detailsRepository: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+
+const store = configureStore({
+  reducer: reducer,
+});
+
+export default store;
